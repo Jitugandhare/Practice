554c700e8c8b9d58445207d7a8e779e2f131971e
Jitu gandhare <jitugandhare@gmail.com>
2025-10-04 09:21:14 +0530

    starting java

---
+++ java/basics/PublicClassVariables.java
@@ -0,0 +1,12 @@
+public class PublicClassVariables {
+    // Public class variables
+    public static String name = "Jitu";
+    public static int age = 27;
+    public static String designation = "Software Development Engineer";
+
+    public static void main(String[] args) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Designation: " + designation);
+    }
+}
