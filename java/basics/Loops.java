ab876af26f52ff5c9b3d9c25bddd05193eb861e6
Jitu gandhare <jitugandhare@gmail.com>
2025-10-04 10:16:24 +0530

    switch statement

---
+++ java/basics/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String [] args){
+        int n=10;
+        for(int i=1;i<n;i++){
+            System.out.println(i);
+        }
+
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
