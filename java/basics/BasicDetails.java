7300204824d0a6506c7f8bb16711954396a0c3bb
Jitu gandhare <jitugandhare@gmail.com>
2025-10-04 09:27:46 +0530

    basic details

---
+++ java/basics/BasicDetails.java
@@ -0,0 +1,15 @@
+public class BasicDetails {
+
+     public static String name="jitu";
+     public static int age=27;
+     public static int favourite_number=7;
+     public static boolean student=false;
+
+     public static void main(String[] args){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Favourite Number: " + favourite_number);
+        System.out.println("Student: " + student);
+
+     }
+}
