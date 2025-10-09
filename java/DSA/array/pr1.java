f7ace6f2e5542ebb5303217b54441fc871cb930b
Jitu gandhare <jitugandhare@gmail.com>
2025-10-09 23:53:46 +0530

    java array

--- java/DSA/array/pr1.java
+++ java/DSA/array/pr1.java
@@ -1,7 +1,9 @@
 public class pr1 {
     public static void main(String[] args) {
         String[] fruits={"banana","apple","papaya","grapes"};
+        for(int i=0;i<fruits.length;i++){
 
-        System.out.println(fruits[1]);
+            System.out.println(fruits[i]);
+        }
     }
 }
