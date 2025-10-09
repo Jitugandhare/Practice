3f908c9a8e954fab79c410917bda9661fb164433
Jitu gandhare <jitugandhare@gmail.com>
2025-10-09 23:57:27 +0530

    java array

--- java/DSA/array/pr1.java
+++ java/DSA/array/pr1.java
@@ -1,9 +1,20 @@
 public class pr1 {
     public static void main(String[] args) {
-        String[] fruits={"banana","apple","papaya","grapes"};
-        for(int i=0;i<fruits.length;i++){
+        String[] fruits = { "banana", "apple", "papaya", "grapes" };
+        for (int i = 0; i < fruits.length; i++) {
 
             System.out.println(fruits[i]);
         }
+
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        ;
+
+        System.out.println("sum:" + sum);
     }
+
 }
